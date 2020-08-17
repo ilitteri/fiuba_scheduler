3c6cf04e3c7ad37b874a4d0200d6a4eff254ec7e
ilitteri <ilitteri@fi.uba.ar>
2020-08-17 20:15:44 -0300

    writing the pdf data to a txt

--- csv_creation.py
+++ csv_creation.py
@@ -2,9 +2,8 @@ import re
 import pdfplumber
 from collections import namedtuple
 
-days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
+#days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
 
-#line_re = r'(\d{4}) CURSO: (\d{1,2}\w?)'
 
 '''def getDay(days, line):
     i = 0
@@ -12,16 +11,19 @@ days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Doming
         i += 1
     return -1 if i == len(days) else days[i]'''
 
-subjectCode_re = r'(\d{4})\s'
-course_re = r'CURSO:\s(\d{1,2}\D?)'
-time_re = r'\d{2}:\d{2}:\d{2}'
-classType_re = r'\d{2}:\d{2}:\d{2}\s(\D{1,3})\s'
-classroom_re = r'\s(\d{3})\s'
-vacancies_re = r'\d{2}:\d{2}:\d{2}\s\D{1,3}\s([3-9][0-9])\s'
-day_re = r'\s(Lunes|Martes|Miércoles|Jueves|Viernes)\s'
-professor_re = r'\s([A-Z|Ñ]+,\s[A-Z|Ñ]+)\s'
-subject_re = r'CURSO:\s\d{1,2}\D?\s([A-Z|\s|.]+)\s[A-Z|Ñ]+,\s[A-Z|Ñ]+\s'
-#location_re = r'\s(\D{2})'
+#line_re = r'(\d{4}) CURSO: (\d{1,2}\D?) ([A-Z| |.]+) ([A-Z|Ñ]+, [A-Z|Ñ]+) (Lunes|Martes|Miércoles|Jueves|Viernes) (\d{2}:\d{2}:\d{2}) (\d{2}:\d{2}:\d{2}) (\D{1,3}) (\d{1,2}) (\d{3})'
+line_re = r'(\d{4}) CURSO: (\d{1,2}\D?) ([A-Z .]+) ([A-ZÑ]{3,}(?: [A-ZÑ])* +, [A-ZÑ]+) '
+
+subjectCode_re = r'(\d{4}) '
+course_re = r'CURSO: (\d{1,2}\D?)'
+subject_re = r'CURSO: \d{1,2}\D? ([A-Z| |.]+) [A-Z|Ñ]+, [A-Z|Ñ]+ '
+professor_re = r' ([A-Z|Ñ]+, [A-Z|Ñ]+) '
+day_re = r' (Lunes|Martes|Miércoles|Jueves|Viernes) '
+time_re = r'(\d{2}:\d{2}:\d{2})'
+classType_re = r'\d{2}:\d{2}:\d{2} (\D{1,3}) '
+vacancies_re = r'\d{2}:\d{2}:\d{2} \D{1,3} (\d{1,2}) ' #
+classroom_re = r' (\d{3}) '
+#location_re = r' (\D{2})'
 
 def read_pdf(file):
     with pdfplumber.open(file) as pdf, open('schedule.csv', 'w') as csv:
@@ -34,11 +36,17 @@ def read_pdf(file):
                     #analyzeFirstLine(line)
                     first_line = False
                 #print(getDay(days, line))
-                print(re.findall(subject_re, line))
-            
+                print(re.findall(line_re, line))
+            break
 
 def main():
     file = 'schedule.pdf'
     read_pdf(file)
 
-main()
+#main()
+with pdfplumber.open('schedule.pdf') as pdf, open('schedule.txt', 'w') as out:
+    pages = pdf.pages
+    first_line = True
+    for page in pages:
+        text = page.extract_text()
+        out.write(text)
