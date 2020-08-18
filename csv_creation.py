24bb6d5f66d4f634100d808e24fad4f75d2bde37
ilitteri <ilitteri@fi.uba.ar>
2020-08-18 03:21:55 -0300

    used a single regex intead of multiple

--- csv_creation.py
+++ csv_creation.py
@@ -1,36 +1,12 @@
-import re
 import pdfplumber
-from collections import namedtuple
-
-#days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
-
-
-'''def getDay(days, line):
-    i = 0
-    while i < len(days) and not days[i] in line:
-        i += 1
-    return -1 if i == len(days) else days[i]'''
-
-#line_re = r'(\d{4}) CURSO: (\d{1,2}\D?) ([A-Z| |.]+) ([A-Z|Ñ]+, [A-Z|Ñ]+) (Lunes|Martes|Miércoles|Jueves|Viernes) (\d{2}:\d{2}:\d{2}) (\d{2}:\d{2}:\d{2}) (\D{1,3}) (\d{1,2}) (\d{3})'
-line_re = r'(\d{4}) CURSO: (\d{1,2}\D?) ([A-Z .]+) ([A-ZÑ]{3,}(?: [A-ZÑ])* +, [A-ZÑ]+) '
-
-subjectCode_re = r'(\d{4}) '
-course_re = r'CURSO: (\d{1,2}\D?)'
-subject_re = r'CURSO: \d{1,2}\D? ([A-Z| |.]+) [A-Z|Ñ]+, [A-Z|Ñ]+ '
-professor_re = r' ([A-Z|Ñ]+, [A-Z|Ñ]+) '
-day_re = r' (Lunes|Martes|Miércoles|Jueves|Viernes) '
-time_re = r'(\d{2}:\d{2}:\d{2})'
-classType_re = r'\d{2}:\d{2}:\d{2} (\D{1,3}) '
-vacancies_re = r'\d{2}:\d{2}:\d{2} \D{1,3} (\d{1,2}) ' #
-classroom_re = r' (\d{3}) '
-#location_re = r' (\D{2})'
 
 def read_pdf(file):
+    trans = str.maketrans('áéíóúäëïöüÁÉÍÓÚÄËÏÖÜ', 'aeiouaeiouAEIOUAEIOU')
     with pdfplumber.open(file) as pdf, open('schedule.csv', 'w') as csv:
         pages = pdf.pages
         first_line = True
         for page in pages:
-            text = page.extract_text()
+            text = page.extract_text().translate(trans)
             for line in text.split('\n'):
                 if first_line:
                     #analyzeFirstLine(line)
@@ -43,10 +19,14 @@ def main():
     file = 'schedule.pdf'
     read_pdf(file)
 
-#main()
+main()
+'''
+trans = str.maketrans('áéíóúäëïöüÁÉÍÓÚÄËÏÖÜ', 'aeiouaeiouAEIOUAEIOU')
 with pdfplumber.open('schedule.pdf') as pdf, open('schedule.txt', 'w') as out:
     pages = pdf.pages
     first_line = True
     for page in pages:
-        text = page.extract_text()
+        text = page.extract_text().translate(trans)
         out.write(text)
+        break
+'''
