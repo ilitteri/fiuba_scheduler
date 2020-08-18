9e99a277d732840cb9717f010a7f031c826065ea
ilitteri <ilitteri@fi.uba.ar>
2020-08-18 18:27:23 -0300

    added: create dataframe module, rename columns module and main function

--- csv_reading.py
+++ csv_reading.py
@@ -1,8 +1,25 @@
 import pandas as pd
 
-csv_file = 'schedule.csv'
-df = pd.read_csv(csv_file)
+def renameColumns(data_frame):
+
+    new_columns = {"Código": "codigo", "Curso": "curso", "Asignatura": "asignatura", "Docentes":"profesores", 
+                "Días": "dias", "Hora Inicio": "hora inicio", "Hora Fin": "hora fin", "Tipo Clase": "tipo clase", 
+                "Vacantes": "vacantes", "Aula": "aula", "Sede": "sede"}
+    df_renamed = data_frame.rename(columns=new_columns)
+
+    return df_renamed
+
+def createDataFrame(csv_file):
+    df = pd.read_csv(csv_file)
+    df = renameColumns(df)
+
+    return df
+
+def main():
+    createDataFrame(csv_file='schedule.csv')
+    dimensions = df.shape
+    analisis_matematico_2 = df[df['Asignatura'].isin(['ANALISIS MATEMATICO II A', 'ANÁLISIS MATEMÁTICO II'])]
+    ensayo_de_pozo_prof = df.loc[df['Asignatura'] == 'ENSAYO DE POZO', 'Docentes']
 
 
-dimensions = df.shape
-print(df.info())
+    print(df_renamed)
