0eafd0fa76540d92124138bc860a1687efe62a39
ilitteri <ilitteri@fi.uba.ar>
2020-08-18 19:03:14 -0300

    replacing rename module

--- csv_reading.py
+++ csv_reading.py
@@ -1,16 +1,8 @@
 import pandas as pd
 
-def renameColumns(data_frame):
-
-    new_columns = {"Código": "codigo", "Curso": "curso", "Asignatura": "asignatura", "Docentes":"profesores", 
-                "Días": "dias", "Hora Inicio": "hora inicio", "Hora Fin": "hora fin", "Tipo Clase": "tipo clase", 
-                "Vacantes": "vacantes", "Aula": "aula", "Sede": "sede"}
-    df_renamed = data_frame.rename(columns=new_columns)
-
-    return df_renamed
-
 def createDataFrame(csv_file):
     df = pd.read_csv(csv_file)
-    df = renameColumns(df)
+    df.columns = ["codigo", "curso", "asignatura", "profesores", "dias", "hora inicio", 
+                "hora fin", "tipo de clase", "vacantes", "aula", "sede"]
 
     return df
