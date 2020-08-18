f0017af7829e8b16eed1d971c937d797cdf4f4f9
ilitteri <ilitteri@fi.uba.ar>
2020-08-18 11:30:27 -0300

    not creating the csv in this module anymore

--- xml_reading.py
+++ xml_reading.py
@@ -1,12 +1,8 @@
-import re
 import pandas as pd
 from lxml import etree
 
 def getData(content):
     return [(element.text or '').strip() for element in content.xpath('//span')]
-
-def createCSV(data_frame, name_file, header, data):
-    data_frame.to_csv(name_file, index=False, header=True)
 
 def readHTML(file):
     with open(file, 'r', encoding='utf-8') as xml_file:
@@ -19,10 +15,8 @@ def readHTML(file):
 
     return header, data
 
-def main():
-    file = 'schedule.xml'
+def main(file):
     header, data = readHTML(file)
     data_frame = pd.DataFrame(data, columns = header)
-    createCSV(data_frame, 'schedule.csv', header, data)
 
-main()
+    return data_frame
