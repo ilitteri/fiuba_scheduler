90566c9094f8513ac68e081f68c5ee385ed18654
ilitteri <ilitteri@fi.uba.ar>
2020-08-18 18:40:21 -0300

    removing main function

--- csv_reading.py
+++ csv_reading.py
@@ -14,12 +14,3 @@ def createDataFrame(csv_file):
     df = renameColumns(df)
 
     return df
-
-def main():
-    createDataFrame(csv_file='schedule.csv')
-    dimensions = df.shape
-    analisis_matematico_2 = df[df['Asignatura'].isin(['ANALISIS MATEMATICO II A', 'ANÁLISIS MATEMÁTICO II'])]
-    ensayo_de_pozo_prof = df.loc[df['Asignatura'] == 'ENSAYO DE POZO', 'Docentes']
-
-
-    print(df_renamed)
