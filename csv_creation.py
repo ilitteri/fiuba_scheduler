240248f1a7f356aa9318885767652e2985ad151a
ilitteri <ilitteri@fi.uba.ar>
2020-08-18 16:12:41 -0300

    merging ex-csv_creation with main (new csv_creation) to separate functionalities

--- csv_creation.py
+++ csv_creation.py
@@ -1,4 +1,14 @@
-import pandas as pd
+import xml_reading
+import csv_creation
 
 def createCSV(data_frame, name_file):
     data_frame.to_csv(name_file, index=False, header=True)
+
+def main():
+    xml_file = 'schedule.xml'
+    df = xml_reading.getDataFrame(xml_file)
+
+    csv_file = f'{xml_file[:xml_file.index(".")]}.csv'
+    createCSV(df, csv_file)
+
+main()
